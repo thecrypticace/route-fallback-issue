99acd397734d86e9dd952eb5a05ee53f793df1e1
Jordan Pittman <jordan@cryptica.me>
2017-11-20 18:58:09 -0500

    Add debug code to encrypt cookies middleware

--- app/Http/Middleware/EncryptCookies.php
+++ app/Http/Middleware/EncryptCookies.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Middleware;
 
+use Closure;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 use Illuminate\Cookie\Middleware\EncryptCookies as Middleware;
 
 class EncryptCookies extends Middleware
@@ -14,4 +17,41 @@ class EncryptCookies extends Middleware
     protected $except = [
         //
     ];
+
+    public function handle($request, Closure $next)
+    {
+        $this->dump("Entering EncryptCookies");
+        
+        $response = parent::handle($request, $next);
+        
+        
+        $this->dump("Leaving EncryptCookies");
+
+        return $response;
+    }
+
+    private function dumpCookies($cookies)
+    {
+        foreach ($cookies as $cookie) {
+            $this->dump([
+                "name" => $cookie->getName(),
+                "value" => $cookie->getValue(),
+            ]);
+        }
+    }
+
+    private function dump($data)
+    {
+        static $cloner;
+        static $dumper;
+
+        $cloner = $cloner ?? new VarCloner;
+        $dumper = $dumper ?? tap(new HtmlDumper, function ($dumper) {
+            $dumper->setDisplayOptions([
+                "maxStringLength" => 10000,
+            ]);
+        });
+
+        $dumper->dump($cloner->cloneVar($data));
+    }
 }
