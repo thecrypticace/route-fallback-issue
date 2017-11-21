c2e972f5c4de5e02eade3c406715aa10b639071b
Jordan Pittman <jordan@cryptica.me>
2017-11-20 19:25:47 -0500

    Update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    RespondWithFallback
+                    ::respondWithRoute
                 </div>
 
                 <div class="links">
